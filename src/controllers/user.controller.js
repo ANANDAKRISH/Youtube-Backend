7264c1b92842d10275bcbe31a92bb902603d57b8
AnandaKrishnan P <anandu235@gmail.com>
2024-07-04 21:53:38 +0530

    user login & logout functionalities added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const refreshToken = user.generateRefreshToken()
+        const accessToken = user.generateAccessToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave:false })
+
+        return {refreshToken , accessToken} // returning an object containing the 2 generated tokens
+        
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating the tokens")
+    }
+}
+
 const registerUser = asyncHandler (async (req,res) => {
     const {fullName,email,password,username} = req.body
     // console.log("full-name : ",fullName);
@@ -54,7 +70,7 @@ const registerUser = asyncHandler (async (req,res) => {
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username.toLowerCase()
+        username: username.toLowerCase() // not sure if toLowerCase is required as in the userschema we have set lowercase: true
     })
 
     const createdUser = await User.findById(user._id).select(
@@ -73,11 +89,84 @@ const registerUser = asyncHandler (async (req,res) => {
 })
 
 const loginUser = asyncHandler (async(req,res) => {
-    const {email,username,password} = req.body
+    const {username,email,password} = req.body
+
+    if(!(username || email)) {
+        throw new ApiError(400,"Username or email is required to login")
+    }
+
+    const user = await User.findOne({
+        $or: [{username} , {email}] // I think we need to use username.toLowerCase() as usernames in db are in lowercase
+    })
+
+    if(!user) {
+        throw new ApiError(404,"User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid) {
+        throw new ApiError(401,"Invalid User Credentials")
+    }
+
+    const {refreshToken , accessToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken") // this is optional , user also 
+    // contains same information but it doesnt have the updated refreshToken field. As we are omitting the 
+    // refreshToken field from loggedInUser , it is fine if we send user instead of loggedInUser. 
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken
+            },
+            "User Logged In Successfully"
+        )
+    )
+
+})
+
+const logoutUser = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken : undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken" , options)
+    .clearCookie("refreshToken" , options)
+    .json(new ApiResponse(200,{},"User Logged Out Successfully"))
+
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
 
